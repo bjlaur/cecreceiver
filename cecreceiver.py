5f89e9c2af85261ffc08f7bdebc38dc1c078b86a
nullstring <nullstring@alarmpi>
2023-10-20 15:38:01 +0000

    incremental checkin

--- cecreceiver.py
+++ cecreceiver.py
@@ -7,11 +7,12 @@ os.environ["PYTHONCEC_DEVICE_TYPE"] = str(CEC_DEVICE_TYPE_AUDIO_SYSTEM)
 
 import sys
 from loguru import logger
-logger.add("output.log", rotation="500 MB", level="INFO", colorize=True)  # Add a file handler
+logger.add("output.log", rotation="500 MB", level="DEBUG")  # Add a file handler
 logger.add(sys.stdout, level="INFO", colorize=True)  # Add a stdout handler
 logger.level("COMMAND", no=21, color="<yellow>")
 logger.level("NOTICE", no=19, color="<green>")
 logger.level("TRAFFIC", no=15, color="<cyan>")
+logger.success("Logging initilized.")
 
 import time
 import cec
@@ -34,7 +35,7 @@ OPCODES = {}
 EVENTS = {}
 
 for key, value in int_constants.items():
-    if key.startswith('CECDEVICE_'):
+    if key.startswith('CECDEVICE_') and key != 'CECDEVICE_UNREGISTERED':
         DEVICES[value] = key[len('CECDEVICE_'):]
     elif key.startswith('CEC_ALERT'):
         ALERTS[value] = key[len('CEC_ALERT'):]
@@ -84,35 +85,49 @@ def printLog(argv):
 
 # Replace as necessary with your own HA entity and services for volume
 def volume_up():
-    pass
+    logger.info("volume up")
 def volume_down():
-    pass
+    logger.info("volume down")
 def volume_mute():
-    pass
+    logger.info("mute")
+def tv_on():
+    logger.info("TV ON")
+def tv_off():
+    logger.info("TV OFF")
 
 def callback(event, *argv):
-    if event == cec.EVENT_COMMAND:
-        command = argv[0]
-        printCommand(command)
-        if command['opcode'] == cec.CEC_OPCODE_REQUEST_ARC_START:
-            logger.success('Reporting ARC Started')
-            cec.transmit(cec.CECDEVICE_TV, cec.CEC_OPCODE_REPORT_ARC_STARTED, '', cec.CECDEVICE_AUDIOSYSTEM)
-    elif event == cec.EVENT_KEYPRESS:
-        code, duration = argv
-        logger.info("keypress", code, duration)
-        if code == 65 and duration == 0:
-            volume_up()
-            logger.info("volume up")
-        elif code == 66 and duration == 0:
-            volume_down()
-            logger.info("volume down")
-        elif code == 67 and duration == 0:
-            volume_mute()            
-            logger.info("mute")
-    elif event == cec.EVENT_LOG:
-        printLog(argv)
-    else:
-        logger.warning("uncategorized event", "event: ", event, "argv: ", argv)
+    try:
+        if event == cec.EVENT_COMMAND:
+            command = argv[0]
+            printCommand(command)
+            if command['opcode'] == cec.CEC_OPCODE_REQUEST_ARC_START:
+                logger.success('Reporting ARC Started')
+                cec.transmit(cec.CECDEVICE_TV, cec.CEC_OPCODE_REPORT_ARC_STARTED, '', cec.CECDEVICE_AUDIOSYSTEM)
+                tv_on()
+            if command['opcode'] == cec.CEC_OPCODE_STANDBY and command['destination'] == cec.CECDEVICE_BROADCAST:
+                #A device has requested all go to standby.
+                logger.info("STANDBY Broadcast sent by " + DEVICES.get(command["initiator"], command["initiator"]))
+                tv_off()
+
+        elif event == cec.EVENT_KEYPRESS:
+            code, duration = argv
+            logger.info(f'keypress {code} {duration}')
+            #logger.info(join('a','b'))
+            if code == 65 and duration == 0:
+                volume_up()
+            elif code == 66 and duration == 0:
+                volume_down()
+            elif code == 67 and duration == 0:
+                volume_mute()            
+        elif event == cec.EVENT_LOG:
+            printLog(argv)
+            level, time, msg = argv
+            if msg == "TV (0): power status changed from 'standby' to 'in transition from standby to on'":
+                tv_on()
+        else:
+            logger.warning(f'uncategorized event. event: {event}, argv: {argv}')
+    except Exception as e:
+        logger.error(f"An error occurred: {str(e)}" + traceback.format_exc())
 
 
 cec.add_callback(callback, cec.EVENT_ALL)
